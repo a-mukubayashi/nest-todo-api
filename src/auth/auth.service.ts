2c9b6c4d6c4d3ca2943167e613f89e36da69a98e
a-mukubayashi <akawasumi92@gmail.com>
2022-09-04 02:03:47 +0900

    feat: auth service and auth controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import { Msg, Jwt } from './interfaces/auth.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+  async signUp(dto: AuthDto): Promise<Msg> {
+    // 第二引数のrounds --> 2の12乗
+    const hashed = await bcrypt.hash(dto.password, 12);
+    try {
+      // user modelに対してcreate操作
+      await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hashedPassword: hashed,
+        },
+      });
+      return {
+        message: 'ok',
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // https://www.prisma.io/docs/reference/api-reference/error-reference#p2002
+        if (error.code === 'P2002') {
+          // Unique error code
+          throw new ForbiddenException('This email is already taken');
+        }
+        throw error;
+      }
+    }
+  }
+
+  async login(dto: AuthDto): Promise<Jwt> {
+    const user = await this.prisma.user.findUnique({
+      // userがdbに存在するかどうか
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Email or password incorrect');
+    // 入力されたpasswordとdbにあるhash化されたpasswordが一致するか
+    const isValid = await bcrypt.compare(dto.password, user.hashedPassword);
+    if (!isValid) throw new ForbiddenException('Email or password incorrect');
+    return this.generateJwt(user.id, user.email);
+  }
+
+  async generateJwt(userId: number, email: string): Promise<Jwt> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    const accessToken = await this.jwt.signAsync(payload, {
+      expiresIn: '5m', // 有効期限5分
+      secret,
+    });
+    return {
+      accessToken,
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // whitelist-->dtoに含まれないフィールドを省いてくれる
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  // get cookie
+  app.use(cookieParser());
+  // csrf対策
+  // client GET:/auth/csrf --> server
+  // client <-- server response: set-cookie: Secret(csrf tokenを生成したsecret key)httpOnly cookie
+  // serverからclientにset-cookieされるので、それ以降requestした時はrequestの中にcookieが含まれる
+  // 正規のサイトからrequest GET:/auth/csrfした時はcsrf tokenをresponseに返す
+  // httpOnlyにするとclient側のjsから読み込めないようになる
+  // client --> server POST: /auth/login
+  // client <-- server set-cookie JWT
+  // client --> server POST: /task
+  // cookie: Secret, JWT, request header: Csrf Token
+  // csrf tokenのhashが一致すれば正規のサイトからのrequestだと認証しDBに追加
+
+  // SameSite = lax: default --> Cookieの送受信ができないCSRF対策でChromeが対策している
+  // SameSite = none cross domainのclient, serverでcookieの送受信が可能。Secure: trueにするとhttps通信のみcookieが使用可能
   await app.listen(3005);
 }
 bootstrap();
